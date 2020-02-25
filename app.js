1941ca4c036a704b0c7ee14f20d4880747688a8b
SelmanDesign09 <selmandesign09@SelmanDn09siMac.fios-router.home>
2020-02-25 15:55:57 -0500

    basic framework

--- app.js
+++ app.js
@@ -4,52 +4,15 @@ var favicon = require('serve-favicon');
 var secure = require('ssl-express-www');
 var app = express();
 
-app.use(secure);
+//app.use(secure);
 
 //favicon
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 //index
 app.get('/', (req, res) => {
   res.render('pages/index', {
-    title: 'Calvin | Product Design'
-  });
-});
-
-//resume
-
-//p5.jssketch
-app.get('/sketch', (req, res) => {
-  res.render('pages/sketch', {
-    title: 'Calvin | sketch'
-  });
-});
-
-//botnet
-app.get('/botnet', (req, res) => {
-  res.render('pages/botnet', {
-    title: 'Calvin | Botnet'
-  });
-});
-
-//botnet
-app.get('/abacus', (req, res) => {
-  res.render('pages/abacus', {
-    title: 'Calvin | Abacus'
-  });
-});
-
-//epiphyte
-app.get('/epiphyte', (req, res) => {
-  res.render('pages/epiphyte', {
-    title: 'Calvin | Epiphyte'
-  });
-});
-
-//bitclaims
-app.get('/bitclaims', (req, res) => {
-  res.render('pages/bitclaims', {
-    title: 'Calvin | Bitclaims'
+    title: 'No Web Without Woment'
   });
 });
 
--- public/JS/style.js
+++ public/JS/style.js
@@ -1,1 +1,12 @@
+const init = function(){
+    let items = document.querySelectorAll('li');
+    for (let i = 0; i < items.length; i++){
+      var back = ["#ff0000","#FFB803","#DC03FF","#03FFFF","#90FF03","FF7C03","#FFB803","#0359FF","#E4E4E4","#03D2FF","#427B52","#413D42","#6D1995","#207FA8","#72FF03","#EFC5F5"];
+      var rand = back[Math.floor(Math.random() * back.length)];
+      items[i].style.background = rand;
+    }
 
+    cssScrollSnapPolyfill()
+}
+
+init();
