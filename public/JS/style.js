5ff2947ab87246709ba5290654000f42063590c3
SelmanDesign09 <selmandesign09@SelmanDn09siMac.fios-router.home>
2020-02-27 12:14:04 -0500

    auto closing and color

--- public/JS/style.js
+++ public/JS/style.js
@@ -1,24 +1,48 @@
+function randomNoRepeats(array) {
+  var copy = array.slice(0);
+  return function() {
+    if (copy.length < 1) { copy = array.slice(0); }
+    var index = Math.floor(Math.random() * copy.length);
+    var item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
+}
+
+var bgcolor = randomNoRepeats(["#ff867c","#ff77a9","#df78ef","#b085f5","#8e99f3","#80d6ff","#73e8ff","#6ff9ff","#64d8cb","#98ee99","#cfff95","#ffff89","#d4e157","#ffee58","#ffca28","#ffa726","#ff7043","#ef5350","#ec407a","#42a5f5","#29b6f6","#26c6da"]);
+
 const init = function(){
     let items = $('li');
     for (let i = 0; i < items.length; i++){
-      var back = ["#FA89FF","#97FF89","#C3EAF5","#ADB0FA","#E6E6E2","#BFD6D3","#BFC6D6","#C3D6BF","#FFAAAA","#AAFFE2","#F2F4BA","#67FFAD","#FFD067"];
-      var rand = back[Math.floor(Math.random() * back.length)];
-      items[i].style.background = rand;
+      items[i].style.background = bgcolor();
+      console.log(bgcolor());
     }
 }
 
 $(".titlecard").click(function(){
-  $(this).children(':nth-child(4)').toggleClass("showcard");
-  $(this).children(':nth-child(1)').toggleClass("rota");
-  $(this).children(':nth-child(3)').children().toggleClass("name");
-  $(this).children(':nth-child(3)').children().children().toggleClass("noname");
+  var title = $(this).children(':nth-child(4)');
+  var ex = $(this).children(':nth-child(1)');
+  var name = $(this).children(':nth-child(3)').children()
+  var core = $('.infocard');
+  if(title.hasClass('showcard')) {
+    $('.infocard').removeClass("showcard");
+    $('.expand').removeClass("rota");
+    $('.invention').removeClass("name");
+  } else {
+    $('.infocard').removeClass("showcard");
+    $('.expand').removeClass("rota");
+    $('.invention').removeClass("name");
+    title.toggleClass("showcard");
+    ex.toggleClass("rota");
+    name.toggleClass("name");
+  }
 });
 
 $(document).ready(function() {
   $(window).scroll(function() {
-    if($(window).scrollTop() > 0) {
+    if($(window).scrollTop() > 30) {
       $('.headl').addClass('small');
-      $('.bodygrid').css("margin-top","120px");
+      $('.bodygrid').css("margin-top","30vh");
       $('.heightcomp').css("height","auto");
       $('.defaulp').css("display","none");
       $('.headlinehold').css("margin","0");
@@ -28,7 +52,6 @@ $(document).ready(function() {
       $('.bodygrid').css("margin-top","81vh");
       $('.defaulp').css("display","block");
       $('.headlinehold').css("margin","auto");
-      console.log("hello world");
     }
   });
 });
